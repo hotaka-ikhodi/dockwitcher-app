61ac0056f82bf59b3d3f065ff71df424bd8057e4
sbermudez <sergio@hotaka.io>
2024-06-20 17:22:07 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import NavigationBar from './components/NavigationBar.vue'
+</script>
+
+<template>
+  <div class="container">
+    <navigation-bar></navigation-bar>
+    <RouterView />
+  </div>
+</template>
