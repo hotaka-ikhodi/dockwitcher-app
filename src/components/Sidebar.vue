3b8d3629742412794d37a84d1e465ecd3599e0d6
sbermudez <sergio@hotaka.io>
2024-07-08 16:17:13 +0200

    WIP: Pagina de drivers

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,5 @@
 <script setup>
-import DockwitcherLogo from './icons/DockwitcherLogo.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
@@ -21,9 +21,9 @@ import DockwitcherLogo from './icons/DockwitcherLogo.vue'
               id="collapse-btn"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#collapseExample"
+              data-bs-target="#collapseCamiones"
               aria-expanded="false"
-              aria-controls="collapseExample"
+              aria-controls="collapseCamiones"
             >
               <img
                 id="collapse-icon"
@@ -32,10 +32,10 @@ import DockwitcherLogo from './icons/DockwitcherLogo.vue'
               />
             </button>
           </div>
-          <div class="collapse" id="collapseExample">
+          <div class="collapse" id="collapseCamiones">
             <div>
-              <a href="#">Camiones</a>
-              <a href="#">Conductores</a>
+              <p><RouterLink to="/trucks">Camiones</RouterLink></p>
+              <p><RouterLink to="/drivers">Conductores</RouterLink></p>
             </div>
           </div>
         </li>
@@ -50,9 +50,9 @@ import DockwitcherLogo from './icons/DockwitcherLogo.vue'
               id="collapse-btn"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#collapseExample"
+              data-bs-target="#collapsePlanificacion"
               aria-expanded="false"
-              aria-controls="collapseExample"
+              aria-controls="collapsePlanificacion"
             >
               <img
                 id="collapse-icon"
@@ -61,102 +61,14 @@ import DockwitcherLogo from './icons/DockwitcherLogo.vue'
               />
             </button>
           </div>
-          <div class="collapse" id="collapseExample">
+          <div class="collapse" id="collapsePlanificacion">
             <div>
-              <a href="#">Panel</a>
-              <a href="#">Recepciones</a>
-              <a href="#">Expediciones</a>
+              <p><a href="#">Panel</a></p>
+              <p><a href="#">Recepciones</a></p>
+              <p><a href="#">Expediciones</a></p>
             </div>
           </div>
         </li>
-        <li>
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="">
-              <img id="planification-icon" src="../assets/images/list-solid.svg" width="15" />
-              Planificacion
-            </div>
-            <button
-              class="btn btn-primary"
-              id="collapse-btn"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseExample"
-              aria-expanded="false"
-              aria-controls="collapseExample"
-            >
-              <img
-                id="collapse-icon"
-                src="../assets/images/icons8-chevron-abajo-24.png"
-                width="15"
-              />
-            </button>
-          </div>
-          <div class="collapse" id="collapseExample">
-            <div>
-              <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="#">Panel</a></li>
-                  <li class="breadcrumb-item">
-                    <a href="../views/PlanificacionView.vue">Planificacion</a>
-                  </li>
-                  <li class="breadcrumb-item">
-                    <a href="../views/DashboardView.vue">Dashboard</a>
-                  </li>
-                </ol>
-              </nav>
-              <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="#">Recepciones</a></li>
-                  <li class="breadcrumb-item">
-                    <a href="../views/PlanificacionView.vue">Planificacion</a>
-                  </li>
-                  <li class="breadcrumb-item">
-                    <a href="../views/IncommingsView.vue">Incommings</a>
-                  </li>
-                </ol>
-              </nav>
-              <nav aria-label="breadcrumb">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item"><a href="#">Panel</a></li>
-                  <li class="breadcrumb-item">
-                    <a href="../views/PlanificacionView.vue">Planificacion</a>
-                  </li>
-                  <li class="breadcrumb-item">
-                    <a href="../views/OutgoingsView.vue">Outgoings</a>
-                  </li>
-                </ol>
-              </nav>
-            </div>
-          </div>
-        </li>
-        <div class="d-flex justify-content-between align-items-center">
-          <div class="">
-            <img id="Camiones-icon" src="../assets/images/list-solid.svg" width="15" />
-            Camiones
-          </div>
-          <button
-            class="btn btn-primary"
-            id="collapse-btn"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapseExample"
-            aria-expanded="false"
-            aria-controls="collapseExample"
-          >
-            <img id="collapse-icon" src="../assets/images/icons8-chevron-abajo-24.png" width="15" />
-          </button>
-        </div>
-        <div class="collapse" id="collapseExample">
-          <div>
-            <nav aria-label="breadcrumb">
-              <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="#">Camiones</a></li>
-                <li class="breadcrumb-item"><a href="../views/TrucksView.vue">Trucks</a></li>
-              </ol>
-            </nav>
-            <p>Drivers</p>
-          </div>
-        </div>
         <li>
           <img id="control-muelle-icon" src="../assets/images/user-nurse-solid.svg" width="24" />
           Control de muelle
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import DashboardView from '@/views/DashboardView.vue'
 import IncommingsView from '@/views/IncommingsView.vue'
 import OutgoingsView from '@/views/OutgoingsView.vue'
 import TrucksView from '@/views/TrucksView.vue'
+import DriversView from '@/views/DriversView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -44,6 +45,11 @@ const router = createRouter({
       path: '/trucks',
       name: 'trucks',
       component: TrucksView
+    },
+    {
+      path: '/drivers',
+      name: 'drivers',
+      component: DriversView
     }
   ]
 })
