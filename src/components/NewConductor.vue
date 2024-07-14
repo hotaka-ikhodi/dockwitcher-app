71faf8383adfb8adaa4cc13994342fd44fbf06fa
DesiTirabosque <88808637+DesireeTirabosque@users.noreply.github.com>
2024-07-14 20:34:39 -0300

    validando campo email

--- src/components/NewConductor.vue
+++ src/components/NewConductor.vue
@@ -1,125 +1,123 @@
-<!--<script setup>
-import { ref } from 'vue'
-const formaciones = ref([])
-const nuevaFormacion = ref('')
-const addFormacion = () => {
-  formaciones.value.push(nuevaFormacion.value)
-  nuevaFormacion.value = ''
-}
-</script>-->
-
 <template>
-    <div>
-      <h2>Formulario Nuevo Conductor</h2>
-    </div>
-    <div class="container">
-      <form class="row g-3 needs-validation" novalidate>
-        <div class="col-md-4">
-          <label for="NombreConductor" class="form-label">Nombre Completo</label>
-          <input type="text" class="form-control" id="NombreConductor" required />
-          <div class="valid-feedback">Looks good!</div>
-          <div class="invalid-feedback">Es necesario ingresar el Nombre completo del conductor</div>
-        </div>
-        <div class="col-md-4">
-          <label for="" class="numIdentificacionConductor"
-            >Número Identificación (DNI, SSN, etc.)</label
-          >
-          <input type="text" class="form-control" id="numIdentificacionConductor" required />
-          <div class="valid-feedback">Looks good!</div>
-          <div class="invalid-feedback">Es necesario ingresar el Nombre completo del conductor</div>
-        </div>
-        <div class="col-md-4">
-          <label for="" class="numLicenciaConductor">Número de Licencia</label>
-          <input type="text" class="form-control" id="numLicenciaConductor" required />
-          <div class="valid-feedback">Looks good!</div>
-          <div class="invalid-feedback">Es necesario ingresar el Nombre completo del conductor</div>
-        </div>
-        <div class="col-md-4">
-          <label for="" class="categLicencia">Categoría de Licencia</label>
-          <input type="text" class="form-control" id="categLicencia" required />
-          <div class="valid-feedback">Looks good!</div>
-          <div class="invalid-feedback">Es necesario ingresar el Nombre completo del conductor</div>
-        </div>
-        <div class="col-md-4">
-          <label for="" class="fechaCaducLicencia">Fecha caducidad de Licencia</label>
-          <input type="date" class="form-control" id="fechaCaducLicencia" required />
-          <div class="valid-feedback">Looks good!</div>
-          <div class="invalid-feedback">Es necesario ingresar el Nombre completo del conductor</div>
-        </div>
-        <div class="col-md-4">
-          <label for="" class="categLicencia">Categoría de Licencia</label>
-          <input type="text" class="form-control" id="categLicencia" required />
-          <div class="valid-feedback">Looks good!</div>
-          <div class="invalid-feedback">Es necesario ingresar el Nombre completo del conductor</div>
-        </div>
-        <div class="col-md-4">
-          <label for="" class="telConductor">Teléfono</label>
-          <input type="text" class="form-control" id="telConductor" />
-          <div class="valid-feedback">Looks good!</div>
-        </div>
-        <div class="col-md-4">
-          <label for="" class="emailConductor">Email</label>
-          <input type="email" class="form-control" id="emailConductor" />
-          <div class="valid-feedback">Looks good!</div>
-        </div>
-        <div class="col-md-4">
-          <label for="" class="formacionesConductor">Formaciones</label>
-          <input type="email" class="form-control" id="formacionesConductor" />
-          <div class="valid-feedback">Looks good!</div>
-        </div>
-        <div class="col-md-4">
-          <label for="" class="observacionesConductor">Observaciones</label>
-          <input type="email" class="form-control" id="observacionesConductor" />
-          <div class="valid-feedback">Looks good!</div>
-        </div>
-        <div class="col-md-4">
-          <label for="" class="experienciaConductor">Experiencia</label>
-          <input type="email" class="form-control" id="experienciaConductor" />
-          <div class="valid-feedback">Looks good!</div>
-        </div>
-        <div class="col-md-4">
-          <label for="" class="disponibilidadConductor">Disponibilidad</label>
-          <input type="email" class="form-control" id="disponibilidadConductor" />
-          <div class="valid-feedback">Looks good!</div>
-        </div>
-        <div class="col-12">
-          <label for="" class="historialConductor">Historial</label>
-          <input type="email" class="form-control" id="historialConductor" />
-          <div class="valid-feedback">Looks good!</div>
-        </div>
-        <div class="col-12">
-          <button class="btn btn-outline-secondary" type="submit">Nuevo</button>
-        </div>
-      </form>
-    </div>
-  </template>
-  <script>
+  <div>
+    <h2>Formulario Nuevo Conductor</h2>
+  </div>
+  <div class="container">
+    <Form class="row g-3" @submit="onSubmit">
+      <div class="col-md-4">
+        <label for="NombreConductor">Nombre Completo</label>
+        <Field name="nombre" type="text" class="form-control" id="NombreConductor" required />
+      </div>
+      <div class="col-md-4">
+        <label for="">Número Identificación (DNI, SSN)</label>
+        <Field
+          name="numIdentificacion"
+          type="text"
+          class="form-control"
+          id="numIdentificacionConductor"
+          required
+        />
+      </div>
+      <div class="col-md-4">
+        <label for="" class="numLicenciaConductor">Número de Licencia</label>
+        <Field
+          name="numLicencia"
+          type="text"
+          class="form-control"
+          id="numLicenciaConductor"
+          required
+        />
+      </div>
+      <div class="col-md-4">
+        <label for="" class="categLicencia">Categoría de Licencia</label>
+        <Field name="catLicencia" type="text" class="form-control" id="categLicencia" required />
+      </div>
+      <div class="col-md-4">
+        <label for="" class="fechaCaducLicencia">Fecha caducidad de Licencia</label>
+        <Field
+          name="fechaCaducidad"
+          type="date"
+          class="form-control"
+          id="fechaCaducLicencia"
+          required
+        />
+      </div>
+      <div class="col-md-4">
+        <label for="" class="categLicencia">Categoría de Licencia</label>
+        <Field
+          name="categoriaLicencia"
+          type="text"
+          class="form-control"
+          id="categLicencia"
+          required
+        />
+      </div>
+      <div class="col-md-4">
+        <label for="" class="telConductor">Teléfono</label>
+        <input type="text" class="form-control" id="telConductor" />
+      </div>
+      <div class="col-md-4">
+        <label for="">Email</label>
+        <Field name="emailCond" type="email" :rules="validateEmail" class="form-control" id="emailConductor" />
+        <ErrorMessage name="emailCond" />
+      </div>
+      <div class="col-md-4">
+        <label for="" class="formacionesConductor">Formaciones</label>
+        <Field name="formacion" type="email" class="form-control" id="formacionesConductor" />
+      </div>
+      <div class="col-md-4">
+        <label for="" class="observacionesConductor">Observaciones</label>
+        <Field name="observacion" type="email" class="form-control" id="observacionesConductor" />
+      </div>
+      <div class="col-md-4">
+        <label for="" class="experienciaConductor">Experiencia</label>
+        <Field name="experiencia" type="email" class="form-control" id="experienciaConductor" />
+      </div>
+      <div class="col-md-4">
+        <label for="" class="disponibilidadConductor">Disponibilidad</label>
+        <Field
+          name="disponibilidad"
+          type="email"
+          class="form-control"
+          id="disponibilidadConductor"
+        />
+      </div>
+      <div class="col-12">
+        <label for="" class="historialConductor">Historial</label>
+        <Field name="historial" type="email" class="form-control" id="historialConductor" />
+      </div>
+      <div class="col-12">
+        <button class="btn btn-outline-secondary" type="submit">Nuevo</button>
+      </div>
+    </Form>
+  </div>
+</template>
 
-
-
-
-  // Example starter JavaScript for disabling form submissions if there are invalid fields
-  ;(function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms).forEach(function (form) {
-      form.addEventListener(
-        'submit',
-        function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        },
-        false
-      )
-    })
-  })()
-  </script>
-  
+<script>
+import { Form, Field, ErrorMessage } from 'vee-validate'
+export default {
+  components: {
+    Field,
+    Form,
+    ErrorMessage,
+  },
+  methods: {
+    onSubmit(values) {
+      console.log(values)
+    },
+    validateEmail(value) {
+      // if the field is empty
+      if (!value) {
+        return 'This field is required'
+      }
+      // if the field is not a valid email
+      const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+      if (!regex.test(value)) {
+        return 'This field must be a valid email'
+      }
+      // All is good
+      return true
+    }
+  }
+}
+</script>
